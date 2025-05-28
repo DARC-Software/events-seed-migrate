e31f6bc3f6261ccd7add31e5d43f5a47212607fb
Devin Cooper <devinraycooper@gmail.com>
2025-05-28 13:37:02 -0400

    troubleshooting query

--- src/main/java/com/wildlighttech/seed_migrate/venue/VenueMapper.java
+++ src/main/java/com/wildlighttech/seed_migrate/venue/VenueMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface VenueMapper {
 
-    @Insert("INSERT INTO venue (name, address, phoneNumber) VALUES #{name}, #{address}, #{phoneNumber}")
+    @Insert("INSERT INTO venue(name, address, phoneNumber) VALUES(#{name}, #{address}, #{phoneNumber})")
     void insertVenue(Venue venue);
 }
